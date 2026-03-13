7f5eb943c2d93809cf0280029c45ceca1993b3b1
Churn3d <gordoncarnes@gmail.com>
2026-03-13 02:56:29 -0400

    begining work on random spawning

--- boid_2d.gd
+++ boid_2d.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var speed = 200.0
 @export var rotation_speed = 1.5
 
+@onready var view_size = get_viewport_rect().size
+
 var rotation_direction = 0
 
 func get_bearing():
@@ -13,3 +15,8 @@ func _physics_process(delta: float) -> void:
 	get_bearing()
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	position.x = wrapf(position.x, 0, view_size.x)
+	position.y = wrapf(position.y, 0, view_size.y)
